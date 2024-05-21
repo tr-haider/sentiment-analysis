709f8c05d5d8e0e4435fcaa1201e3f65309553a3
tr-haider <mhaidershahab@technologyrivers.com>
2024-05-21 19:26:31 +0500

    sentiment-analysis-v9

--- main.py
+++
@@ -1,4 +0,0 @@
-import nltk
-nltk.download('vader_lexicon', download_dir='./nltk_data')
-# nltk.download('stopwords', download_dir='./nltk_data')
-# nltk.download('punkt', download_dir='./nltk_data')
