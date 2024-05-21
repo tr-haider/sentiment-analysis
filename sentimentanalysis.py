d7a86c209a83ca134c525d20e35fcacd066fea3d
tr-haider <mhaidershahab@technologyrivers.com>
2024-05-21 18:36:40 +0500

    sentiment-analysis-v7

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -30,7 +30,7 @@ def perform_ner(text):
 
 # Function to perform sentiment analysis
 def perform_sentiment_analysis(text):
-    sentiment_classifier = pipeline(task="sentiment-analysis",model="distilbert-base-uncased-finetuned-sst-2-english")
+    sentiment_classifier = pipeline("sentiment-analysis")
     preds = sentiment_classifier(text)
     preds = [{"score": round(pred["score"], 4) * 100, "label": pred["label"]} for pred in preds]
     return preds
